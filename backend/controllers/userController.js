7fe2f8a439859a4f257bd2a498e278074d57e621
Muzammil <muzammilarif310@gmail.com>
2025-03-15 14:34:06 +0500

    new

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,71 +1,109 @@
-import validator from 'validator'
-import bcrypt from 'bcrypt'
-import userModel from '../models/User.js'
-import jwt from 'jsonwebtoken'
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import userModel from "../models/User.js";
+import jwt from "jsonwebtoken";
+import { v2 as cloudinary } from "cloudinary";
 
 //API to register user
-const registerUser = async (req,res) => {
+const registerUser = async (req, res) => {
   try {
-    const {name,email, password} = req.body 
-    if(!name || !email ||!password){
-      return res.json({success:"false",message: "Missing Details"})
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.json({ success: "false", message: "Missing Details" });
     }
     //Validating email format
-    if(!validator.isEmail(email)){
-      return res.json({success:"false",message: "Invalid Email"})
+    if (!validator.isEmail(email)) {
+      return res.json({ success: "false", message: "Invalid Email" });
     }
     //Validating strog password
-    if(password.length < 8){
-      return res.json({success:"false",message: "Password should be at least 8 characters"})
+    if (password.length < 8) {
+      return res.json({
+        success: "false",
+        message: "Password should be at least 8 characters",
+      });
     }
     //Hashing password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const userData = {
       name,
       email,
-      password: hashedPassword
-    }
+      password: hashedPassword,
+    };
 
-    const newUser = new userModel(userData)
-    const user = await newUser.save()
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
 
-    const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
-    res.json({success:'true', token}) 
-    
-    
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ success: "true", token });
   } catch (error) {
     console.log(error);
     res.json({ success: "false", message: error.message });
-    
   }
-}
+};
 
-
-//APi for user Login 
-const loginUser = async (req,res) => {
+//APi for user Login
+const loginUser = async (req, res) => {
   try {
-    const {email,password} = req.body
-    const user = await userModel.findOne({email})
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
 
-    if(!user){
-      return res.json({sucess:'false',message:'user does not exist'})
+    if (!user) {
+      return res.json({ sucess: "false", message: "user does not exist" });
     }
-    const isMatch = await bcrypt.compare(password,user.password)
+    const isMatch = await bcrypt.compare(password, user.password);
 
-    if(isMatch){
-      const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
-      res.json({success:'true',token})
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: "true", token });
     } else {
-      res.json({success:'false',message:'Invalid Password'})
+      res.json({ success: "false", message: "Invalid Password" });
     }
-    
   } catch (error) {
     console.log(error);
     res.json({ success: "false", message: error.message });
   }
-}
+};
+//Api to get user profile data
+const getProfile = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const userData = await userModel.findById(userId).select("-password");
+    res.json({ success: true, userData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: "false", message: error.message });
+  }
+};
+//Api to update user profile data
+const updateProfile = async (req, res) => {
+  try {
+    const { userId, name, phone, address, dob, gender } = req.body;
+    const imageFile = req.file;
+    if (!name || !phone || !dob || !gender) {
+      return res.json({ success: false, message: "Data Missing" });
+    }
+    await userModel.findByIdAndUpdate(userId, {
+      name,
+      phone,
+      address: JSON.parse(address),
+      dob,
+      gender,
+    });
+    if (imageFile) {
+      //upload Image to Cloudinary
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+        resource_type: "image",
+      });
+      const imageURL = imageUpload.secure_url;
+      await userModel.findByIdAndUpdate(userId, { image: imageURL });
+    }
+    res.json({ success: true, message: "Profile Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-
-export {registerUser,loginUser}
+export { registerUser, loginUser, getProfile, updateProfile };
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-refresh/only-export-components */
@@ -12,8 +13,8 @@ const AppContextProvider = (props) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const [doctors, setDoctors] = useState([]);
-  const [token, setToken] = useState(localStorage.getItem('token') || ''); // Initialize token from localStorage
-
+  const [token, setToken] = useState(localStorage.getItem("token") || ""); // Initialize token from localStorage
+  const [userData, setUserData] = useState(false);
   const getDoctorsData = async () => {
     try {
       const { data } = await axios.get(backendUrl + "/api/doctor/list");
@@ -27,18 +28,42 @@ const AppContextProvider = (props) => {
       toast.error(error.message);
     }
   };
-
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/get-profile", {
+        headers: { token },
+      });
+      if (data.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
   const value = {
     doctors,
     currencySymbol,
     token,
     setToken,
     backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData,
   };
 
   useEffect(() => {
     getDoctorsData();
   }, []);
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
+    } else {
+      setUserData(false);
+    }
+  }, [token]);
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>

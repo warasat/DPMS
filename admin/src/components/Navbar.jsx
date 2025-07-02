239cad9ccdd3306ab15836c1017c08b2d03cfb2e
Warasast <warasatwarraich@gmail.com>
2025-07-03 00:00:10 +0500

    .

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { AdminContext } from "../context/AdminContext";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { DoctorContext } from "../context/DoctorContext";
+import { FaUserMd } from 'react-icons/fa';
 
 const Navbar = () => {
   const { aToken, setAToken } = useContext(AdminContext);
@@ -20,9 +21,9 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b  bg-blue-900 text-white">
+    <div className="flex justify-between items-center px-4 sm:px-4 py-3 p- border-b  bg-blue-900 text-white">
       <div className="flex items-center gap-2 text-xs">
-        <a className="btn btn-ghost text-xl">Doctor & Patient</a>
+        <a className="btn btn-ghost text-xl"><FaUserMd className="inline-block mr-2" /> MediConnect_Doctor & Patient</a>
         <p className="border px-2.5 py-0.5  rounded-full border-white-500 text-white-600 ">
           {aToken ? "Admin" : "Doctor"}
         </p>
